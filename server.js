e35aba3526232a9bb5f2d55b309b548a8f4d882c
goodwinds <goodwinds@users.noreply.github.com>
2016-06-05 02:37:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var pub = __dirname + '/public',
     clientSecret = '2e1236b23c43441eb696ba497cd06c3c';
 */
 var clientID = 'e26a11d8d2c3468c840f279beb2ab564',
-    accessToken= '1570173125.e26a11d',
+    accessToken= '1570173125',
     clientSecret = 'fb782789243447a88f22f81b9740fce5';
 /**
  * Set the configuration
